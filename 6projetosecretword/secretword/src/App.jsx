47d52b3c8c095fbe8777b9b016d678b6e48aa067
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-05-12 17:54:16 -0300

    s6 - ok -- secret word

--- 6projetosecretword/secretword/src/App.jsx
+++ 6projetosecretword/secretword/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
   const [guesses, setGuesses] = useState(3)
   const [score, setScore] = useState(0)
   
-  function pickCategoryAndWord() {
+  const pickCategoryAndWord = useCallback(() => {
     // pick random category
     const categories = Object.keys(words)
     const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
@@ -42,33 +42,86 @@ function App() {
     // console.log(word)
 
     return {category, word}
-  }
+  }, [words])
 
   // start game
-  function startGame() {
+  // usamos useCallback para consertar as dependencias do useEffect caso seja função
+  const startGame = useCallback(() => {
+    // clear all letters
+    clearLetterStates()
+
     const {category, word} = pickCategoryAndWord()
-    console.log(category, word)
+    // console.log(category, word)
 
     // create array letters
     let wordLetters = word.split("")
-
     wordLetters = wordLetters.map(l => l.toLowerCase())
-    console.log(wordLetters)
+    // console.log(wordLetters)
 
     setPikedCategory(category)
     setPikedWord(word)
     setLetters(wordLetters)
 
     setGameStage(stages[1].name)
-  }
+  }, [pickCategoryAndWord])
 
   // process letter input
-  function verifyLetter() {
-    setGameStage(stages[2].name)
+  function verifyLetter(letter) {
+    const normalizedLetter = letter.toLowerCase()
+
+    // if letter has already been utilized
+    if (guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)) {
+      return
+    }
+
+    // push guessed letter or remove a guess
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters, normalizedLetter
+      ])
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters, normalizedLetter
+      ])
+      setGuesses((actualGuesses) => actualGuesses - 1)
+    }
+    // console.log(guessedLetters)
+    // console.log(wrongLetters)
   }
+
+  function clearLetterStates() {
+    setGuessedLetters([])
+    setWrongLetters([])
+  }
+
+  // fica monitorando a variável guesses a cada vez que ela muda
+  // check if guesses ended
+  useEffect(() => {
+    if (guesses <= 0) {
+      // reset all states
+      clearLetterStates()
+      setGameStage(stages[2].name)
+    }
+  }, [guesses])
+
+  // check win condition
+  useEffect(() => {
+    const uniqueLetters = [...new Set(letters)]
+    
+    // win condition
+    if (guessedLetters.length === uniqueLetters.length) {
+      // add score
+      setScore((actualScore) => actualScore += 100)
+      // restart game with new word
+      startGame()
+    }
+  }, [guessedLetters, letters, startGame])
 
   // restarts game
   function restartGame() {
+    setScore(0)
+    setGuesses(3)
+
     setGameStage(stages[0].name)
   }
 
@@ -80,8 +133,12 @@ function App() {
         pickedCategory={pickedCategory}
         pickedWord={pickedWord}
         letters={letters}
+        guessedLetters={guessedLetters}
+        wrongLetters={wrongLetters}
+        guesses={guesses}
+        score={score}
         />}
-      {gameStage === 'end' && <GameOver restartGame={restartGame} />}
+      {gameStage === 'end' && <GameOver restartGame={restartGame} score={score}/>}
     </div>
   );
 }
--- 6projetosecretword/secretword/src/components/GameOver.jsx
+++ 6projetosecretword/secretword/src/components/GameOver.jsx
@@ -1,9 +1,10 @@
 import './GameOver.css'
 
-function GameOver({restartGame}) {
+function GameOver({restartGame, score}) {
   return ( 
     <div className='go'>
-      <h2>Game Over</h2>
+      <h1>Fim de Jogo</h1>
+      <h2>A sua pontuação foi: <span>{score}</span></h2>
       <button onClick={restartGame}>Reiniciar Jogo</button>
     </div>
    );
